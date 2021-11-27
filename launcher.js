941d6558d7b43c4f066009ef0617dd3d9443c8f9
cjcb0607 <91701797+cjcb0607@users.noreply.github.com>
2021-11-27 10:41:26 -0600

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,37 @@
+class Launcher {
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        ///////////////////////////////////////////
+        // MISS B: no existe "this.sling" en este código :o qué debería de ser? n.n 
+        World.add(world, this.launcher);
+    }
+
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    fly(){
+        this.launcher.bodyA = null;
+    }
+    
+
+    display(){
+        // MISS B: esta linea sobraba: image(this.launcher, 200, 20);
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke ("#301608");
+            strokeWeight(4);
+            line(pointA.x-25,pointA.y, pointB.x+30,pointB.y);
+            pop();
+        }
+    }
+    
+}
